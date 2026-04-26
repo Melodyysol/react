ed571fcdbd281127c14fdfe04c2536a93e011727
Melodyysol <issaabdulwaris212@gmail.com>
2026-04-26 10:36:59 +0100

    Make create order button work

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -48,7 +48,7 @@ function CheckoutPage({ cart, loadCart }) {
         <div className="checkout-grid">
 
           <OrderSummary cart={cart} loadCart={loadCart} deliveryOptions={deliveryOptions} />
-          <PaymentSummary paymentSummary={paymentSummary} />
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
         </div>
       </div>
     </>
